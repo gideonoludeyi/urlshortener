b1dd20bbcf835964f69a90d7c185c5a3ecb76771
gideonoludeyi <gooludeyi@gmail.com>
2022-05-24 20:39:33 -0400

    feat: implement Client using sqlite3

--- urlshortener/user/users_db.py
+++ urlshortener/user/users_db.py
@@ -1,18 +1,22 @@
+from ..client.sqliteclient import SQLiteClient
+
 from ..client.base import Client
-from ..client.inmemoryclient import InMemoryClient
 
-client = InMemoryClient()
+# from ..client.inmemoryclient import InMemoryClient
 
-client.set('johndoe@example.com', {
-    "email": "johndoe@example.com",
-    "hashed_password": "$2b$12$TtB2fBC0v4K1PfCwteiSD.Pd9Xuopkbl3K2hFll5/rj6wZw3yVzuW",
-})
+# client = InMemoryClient()
 
-client.set('alice@example.com', {
-    "email": "alice@example.com",
-    "hashed_password": "$2b$12$1uAZBYM2t7NmBDuS67j.P.lln1SDnTcb5ZTp.yfxkuDBbrJZYuy.y",
-})
+# client.set('johndoe@example.com', {
+#     "email": "johndoe@example.com",
+#     "hashed_password": "$2b$12$TtB2fBC0v4K1PfCwteiSD.Pd9Xuopkbl3K2hFll5/rj6wZw3yVzuW",
+# })
+
+# client.set('alice@example.com', {
+#     "email": "alice@example.com",
+#     "hashed_password": "$2b$12$1uAZBYM2t7NmBDuS67j.P.lln1SDnTcb5ZTp.yfxkuDBbrJZYuy.y",
+# })
 
 
 def users_db() -> Client:
-    return client
+    with SQLiteClient(table_name='users', key_name='email') as client:
+        yield client
